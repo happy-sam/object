521caebdeec24adec0e554ed9ced415b9e583a78
Stanisław Romek <info@happy.ovh>
2019-07-01 20:36:42 +0200

    Korekta

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const Car = function (brand, year, color) {
-    this.brand = brand;
-    this.year = year;
-    this.color = color;
-    this.property = function (e) {
-        console.log(`Klucz to ${e}, a jego wartość to ${this[e]}`)
+// const Car = function (brand, year, color) {
+//     this.brand = brand;
+//     this.year = year;
+//     this.color = color;
+//     this.property = function (e) {
+//         console.log(`Klucz to ${e}, a jego wartość to ${this[e]}`)
+//     }
+// }
+
+// let audi = new Car("audi", 2010, "czerwony");
+
+// audi.property("year")
+
+
+const car = {
+    brand: "audi",
+    color: "czerwony",
+    year: 2015
+}
+
+let getCarValue = function (e) {
+
+    if (e == "brand") {
+        return car.brand;
+    } else if (e == "color") {
+        return car.color;
+    } else if (e == "year") {
+        return car.year;
     }
 }
 
-let audi = new Car("audi", 2010, "czerwony");
+console.log(getCarValue("color"));
 
-audi.property("year")
+// można by zamiast if-a zostosować switch
